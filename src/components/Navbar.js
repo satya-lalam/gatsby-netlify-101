9aa8350e248a1fcecc08afd421c365691b18b061
slalam <satyakumar.lalam@unmc.edu>
2018-04-26 12:10:56 -0500

    core pages renaming

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import gpctr_logo from '../img/gpctr_logo.png'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -10,7 +10,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={gpctr_logo} alt="IDeA CTR" style={{ width: '88px' }} />
           </figure>
         </Link>
       </div>
@@ -25,7 +25,7 @@ const Navbar = () => (
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
